2df10703d5b16f13b00d3929554246494b12194f
Faisal Hakim <faisalhakim47@gmail.com>
2024-06-11 22:04:05 +0700

    compact done

--- scripts/compact-data.mjs
+++ scripts/compact-data.mjs
@@ -10,10 +10,11 @@ import url from 'node:url';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const VERSION = 1;
-const NUM_OF_TIMES = 366;
-// const NUM_OF_REGENCIES = 517;
-const NUM_OF_REGENCIES = 600;
-const NUM_OF_PRAYER_TIMES = 8;
+const NUM_OF_TIME_DIFFS = 366;
+const NUM_OF_REGENCIES = 517;
+// const NUM_OF_PRAYER_TIMES = 8;
+const NUM_OF_PRAYER_TIMES = 7; // minus imsak dari fact check bahwa imsak selalu 10 menit sebelum subuh
+const TIME_DIFF_GROUP_SIZE = 4;
 
 /** @type {JadwalSholatData} */
 const { timestamp, prayerTimeScheduleMap } = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/jadwal-sholat.json'), { encoding: 'utf8' }));
@@ -25,10 +26,14 @@ const magicBytes = Buffer.from('AWQTSHLT');
 const headerSize = magicBytes.length
   + 2 // store version in 2 bytes
   + 2 // store number of regencies in 2 bytes
-  + 2 // store number of diff schedules in 2 bytes
+  + 2 // store number of time diffs in 2 bytes
+  + 1 // store time diff group size in 1 byte
+  + 1 // store number of prayer times in 1 byte
 
-const rowSize = 2 // baseMinuteOfDay
-  + (NUM_OF_PRAYER_TIMES * (NUM_OF_TIMES / 2)); // each schedule is 4 bit, so 2 schedules per byte
+const regencyScheduleSize = 2 // baseMinuteOfDay
+  + Math.ceil(NUM_OF_TIME_DIFFS / TIME_DIFF_GROUP_SIZE); // kita dapat menyimpan 4 time diff dalam 8 bit, 2 bit merepresentasikan 4 states: -2, -1, 0, 1.
+
+const rowSize = NUM_OF_PRAYER_TIMES * regencyScheduleSize;
 
 const contentSize = NUM_OF_REGENCIES * rowSize;
 
@@ -46,9 +51,15 @@ index += 2;
 // Write number of regencies
 buffer.writeUInt16BE(NUM_OF_REGENCIES, index);
 index += 2;
-// Write number of diff schedules
-buffer.writeUInt16BE(NUM_OF_TIMES, index);
+// Write number of time diffs
+buffer.writeUInt16BE(Math.ceil(NUM_OF_TIME_DIFFS / TIME_DIFF_GROUP_SIZE), index);
 index += 2;
+// Write time diff group size
+buffer.writeUInt8(4, index);
+index += 1;
+// Write number of prayer times
+buffer.writeUInt8(NUM_OF_PRAYER_TIMES, index);
+index += 1;
 
 const provinces = Object.keys(prayerTimeScheduleMap);
 
@@ -75,34 +86,69 @@ for (const [provinceIndex, province] of provinces.entries()) {
       return a.baseMinuteOfDay - z.baseMinuteOfDay;
     });
 
+    // Fact Check: Apakah imsak selalu 10 menit sebelum subuh?
+    const imsak = prayerTimes[0];
+    const subuh = prayerTimes[1];
+
+    if (imsak.baseMinuteOfDay !== subuh.baseMinuteOfDay - 10) {
+      throw new Error('Imsak is not always 10 minutes before subuh.');
+    }
+
+    for (const [scheduleIndex, { timeDiff }] of subuh.schedules.entries()) {
+      const imsakSchedule = imsak.schedules[scheduleIndex];
+
+      if (timeDiff !== imsakSchedule.timeDiff) {
+        throw new Error('Imsak schedule is not equal to subuh schedule.');
+      }
+    }
+
     metadata += '\t';
     metadata += `${regency}`;
 
-    if (prayerTimes.length !== NUM_OF_PRAYER_TIMES) {
-      throw new Error('Number of prayer times is not equal to 8.');
+    const includedPrayerTimes = prayerTimes.filter(function (_, index) {
+      // Skip imsak
+      if (index === 0) {
+        return false;
+      }
+      return true;
+    });
+
+    if (includedPrayerTimes.length !== NUM_OF_PRAYER_TIMES) {
+      throw new Error(`Number of prayer times is not equal to ${NUM_OF_PRAYER_TIMES}.`);
     }
 
-    for (const [prayerTimeIndex, { baseMinuteOfDay, schedules }] of prayerTimes.entries()) {
-      if (schedules.length !== NUM_OF_TIMES) {
+    prayerTimeLoop:
+    for (const [prayerTimeIndex, { baseMinuteOfDay, schedules }] of includedPrayerTimes.entries()) {
+      if (schedules.length !== NUM_OF_TIME_DIFFS) {
         throw new Error('Number of schedules is not equal to 366.');
       }
 
       buffer.writeUInt16BE(baseMinuteOfDay, index);
       index += 2;
 
-      const schedulePairs = schedules.reduce((schedulePairs, schedule, index) => {
-        if (index % 2 === 0) {
-          schedulePairs.push([schedule.timeDiff]);
-        } else {
-          schedulePairs[schedulePairs.length - 1].push(schedule.timeDiff);
-        }
-        return schedulePairs;
-      }, /** @type {Array<Array<number>>} */ ([]));
+      const timeDiffs = schedules.map(function (schedule) {
+        return schedule.timeDiff;
+      });
 
-      for (const schedulePair of schedulePairs) {
-        const [left, right] = schedulePair;
-        // console.log(index, left, right, (left << 4) | right, left << 4, right);
-        buffer.writeInt8((left << 4) | right, index);
+      for (const timeDiff of timeDiffs) {
+        if (![-2, -1, 0, 1].includes(timeDiff)) {
+          throw new Error('Time diff is not in range of -2 to 1.');
+        }
+      }
+
+      const adjustedTimeDiffs = timeDiffs.map(function (timeDiff) {
+        return timeDiff + 2;
+      });
+
+      for (let timeDiffIndex = 0; timeDiffIndex < adjustedTimeDiffs.length; timeDiffIndex += 4) {
+        const first = adjustedTimeDiffs[timeDiffIndex];
+        const second = adjustedTimeDiffs[timeDiffIndex + 1];
+        const third = adjustedTimeDiffs[timeDiffIndex + 2] ?? 0;
+        const fourth = adjustedTimeDiffs[timeDiffIndex + 3] ?? 0;
+
+        const value = (first << 6) | (second << 4) | (third << 2) | fourth;
+
+        buffer.writeUInt8(value, index);
         index += 1;
       }
     }
@@ -111,9 +157,13 @@ for (const [provinceIndex, province] of provinces.entries()) {
   }
 }
 
-// if (regencyCount !== NUM_OF_REGENCIES) {
-//   throw new Error('Number of regencies is not equal to 517.');
-// }
+if (regencyCount !== NUM_OF_REGENCIES) {
+  throw new Error('Number of regencies is not equal to 517.');
+}
+
+if (index !== bufferSize) {
+  throw new Error(`Expected index to be ${bufferSize}, but got ${index}.`);
+}
 
 fs.writeFileSync(path.join(__dirname, '../data/jadwal-sholat.bin'), buffer);
 fs.writeFileSync(path.join(__dirname, '../data/jadwal-sholat.metadata'), metadata, { encoding: 'utf8' });
